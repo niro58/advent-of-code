40060e98be340951bf564ed9a75d2c7304ddcb8a
Nichita Roilean <roilenic@fit.cvut.cz>
2023-12-21 23:35:19 +0100

    Day_3 first part

--- Day_3/main.cpp
+++ Day_3/main.cpp
@@ -1,70 +1,79 @@
 #include <iostream>
 #include <vector>
-#include <cstring>
+#include <map>
+#include <algorithm>
+#include <string>
+#include <sstream>
 
 using namespace std;
-bool CheckAdjacentCellsValidity(vector<char*> board, int xPos, int yPos, int offset, int sum) {
-    //cout << "--------------" << sum << "--------------" << endl;
-    //cout << offset << endl;
-    //cout << xPos - offset << " " << xPos + 1 << endl;
-    for(int y = yPos - 1; y <= yPos + 1; y++){
-        if(y < 0 || y >= board.size()){
-            continue;
-        }
-        for(int x = xPos - offset - 1; x <= xPos + 1; x++){
-            if(x < 0 || x >= strlen(board[y])){
+
+int yLen = 0;
+int xLen = 0;
+
+bool CheckAdjacentCells(char board[256][256], int yPos, int xPos) {
+    for(int y = yPos - 1; y <= yPos + 1; y++) {
+        for(int x = xPos - 1; x <= xPos + 1; x++) {
+            bool isOutOfBounds = y < 0 || y >= yLen || x < 0 || x >= xLen;
+            bool isValidCharacter = (board[y][x] < '0' || board[y][x] > '9') && board[y][x] != '.';
+            if(isOutOfBounds){
                 continue;
             }
-            cout << "Checking " << y << " " << x << " Char: " << board[y][x] <<endl;
-            if(board[y][x] != '.' && !isdigit(board[y][x])){
-                cout << sum << " ! Valid at " << y << " " << x << " Char: " << board[y][x] <<endl;
+            if(isValidCharacter) {
                 return true;
             }
         }
     }
-
     return false;
 }
 int main() {
-    vector<char*> board; // Using a vector to store rows
-    char tempLine[1000]; // Temporary array to store each line
+    char board[256][256];
+    string line;
 
-    while (cin.getline(tempLine, 1000)) {
-        int lineLength = strlen(tempLine);
-        char* row = new char[lineLength + 1]; // +1 for the null terminator
-        strcpy(row, tempLine);
-        board.push_back(row);
+    while (getline(cin, line)) {
+        for(int i = 0; i < line.length(); i++) {
+            board[yLen][i] = line[i];
+            if(i > xLen){
+                xLen = i;
+            }
+        }
+        yLen++;
     }
-    int sum = 0;
-    for(int y = 0; y < board.size(); y++){
-        int tempNumber = 0;
-        int length = 0;
-        for(int x = 0; x < strlen(board[y]); x++){
-            char cellValue = board[y][x];
-            if(tempNumber != 0){
-                length++;
-            }
-            if(isdigit(cellValue)){
-                tempNumber *= 10;
-                tempNumber += cellValue - '0';
-            }
-            if(tempNumber != 0 && (!isdigit(cellValue) || x == strlen (board[y]) - 1)){
-                // check adjacent validity, input : board ,x, y and length of tempnumber
-                if(CheckAdjacentCellsValidity(board, x, y, length, tempNumber)){
-                    sum += tempNumber;
+    int result = 0;
+    for(int y = 0; y<  yLen; y++) {
+        int temp = 0;
+        bool isValidNumber = false;
+        for(int x = 0; x < xLen; x++) {
+            if(board[y][x] >= '0' && board[y][x] <= '9'){
+                temp *= 10;
+                temp += board[y][x] - '0';
+
+                if(CheckAdjacentCells(board, y, x)){
+                    isValidNumber = true;
                 }
-                length = 0;
-                tempNumber = 0;
+            }
+            if(board[y][x] < '0' || board[y][x] > '9' || x == xLen - 1){
+                if(temp > 0 && isValidNumber){
+                    //remove it from board
+                    for(int i = x - 1; i >= 0; i--) {
+                        if(board[y][i] >= '0' && board[y][i] <= '9'){
+                            board[y][i] = '.';
+                        } else {
+                            break;
+                        }
+                    }
+                    result += temp;
+                    isValidNumber = false;
+                }
+                temp = 0;
+
             }
         }
     }
-    cout << "--------------------------" << endl;
-    for (char* row : board) {
-        cout << row << endl;
+    for(int y = 0; y < yLen; y++) {
+        for(int x = 0; x < xLen; x++) {
+            cout << board[y][x];
+        }
+        cout << endl;
     }
-    for (char* row : board) {
-        delete[] row;
-    }
-    cout << sum << endl;
-    return 0;
+    cout << result << endl;
 }
