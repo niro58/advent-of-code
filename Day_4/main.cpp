51b50f06923e9ff94d66c05afdd2543611182952
Nichita Roilean <roilenic@fit.cvut.cz>
2023-12-23 10:28:09 +0100

    Day 4 - 2nd star

--- Day_4/main.cpp
+++ Day_4/main.cpp
@@ -2,88 +2,48 @@
 #include <map>
 #include <sstream>
 #include <vector>
+#include <algorithm>
 using namespace std;
-void firstV(){
-    //string line;
-    //int sum = 0;
-    //int index = 0;
-    //map<int, int> timesToPlay;
-    //while (getline(cin, line)) {
-    //    string allNumbers = line.substr(line.find(':') + 1, line.length());
-    //    string winningNumbers = allNumbers.substr(0, allNumbers.find('|') - 1);
-    //    string matchingNumbers = allNumbers.substr(allNumbers.find('|') + 1, allNumbers.length());
-//
-    //    stringstream ss;
-    //    list<int> winningNumbersList;
-//
-    //    ss << winningNumbers;
-    //    string temp;
-    //    while (ss >> temp) {
-    //        winningNumbersList.push_back(stoi(temp));
-    //    }
-//
-    //    ss.clear();
-    //    ss << matchingNumbers;
-    //    int matches = 0;
-    //    list<int> matchingNumbersList;
-    //    while (ss >> temp) {
-    //        matchingNumbersList.push_back(stoi(temp));
-//
-    //    }
-    //}
-}
 
 int main() {
-    map<int,int> timesToPlay;
-    timesToPlay.insert(pair<int,int>(0,1));
+    map<int,int> scratchCards;
     string line;
     int index = 0;
     int sum = 0;
     while(getline(cin,line)) {
+        scratchCards[index] += 1;
+
         string allNumbersRaw = line.substr(line.find(':') + 1, line.length());
         string winningNumbersRaw = allNumbersRaw.substr(0, allNumbersRaw.find('|') - 1);
         string matchingNumbersRaw = allNumbersRaw.substr(allNumbersRaw.find('|') + 1, allNumbersRaw.length());
 
-        stringstream ss;
+        stringstream winningNumbers;
+        stringstream matchingNumbers;
 
         vector<int> winningNumbersList;
-        ss << winningNumbersRaw;
+        winningNumbers << winningNumbersRaw;
+        matchingNumbers << matchingNumbersRaw;
         string temp;
-        while (ss >> temp) {
+        while (winningNumbers >> temp) {
             winningNumbersList.push_back(stoi(temp));
         }
-        ss.clear();
-        ss << matchingNumbersRaw;
         int matches = 0;
-        cout << "__________________________" << timesToPlay[index] << endl;
-        while (ss >> temp) {
-            for (int i : winningNumbersList) {
-                if (stoi(temp) == i) {
-                    matches++;
-                }
+
+        while (matchingNumbers >> temp) {
+            int num = stoi(temp);
+            if(find(winningNumbersList.begin(), winningNumbersList.end(), num) != winningNumbersList.end()) {
+                matches++;
+                scratchCards[index + matches] += scratchCards[index];
             }
         }
-        int playedTimes = timesToPlay[index];
-        sum += timesToPlay[index];
+        sum += scratchCards[index];
+        //print matches
+        cout << "Repeats : " << scratchCards[index] << endl;
+        cout << "Matches : " << matches << endl;
+        cout << "____________________" << endl;
 
-        timesToPlay.erase(timesToPlay[index]);
-
-
-        cout << "TOTAL MATCHES: " << matches << " Played times : " << playedTimes << endl;
-        cout << "Before " << timesToPlay[index + 1] << endl;
-
-        //first element += 1;
-        timesToPlay[index + 1] += 1;
-        for(int i = 0; i < matches; i++){
-            timesToPlay[index + i] += 1 * playedTimes;
-        }
-        cout << "After " << timesToPlay[index + 1] << endl;
         index++;
-        //print timestoplay map
-        for(auto i : timesToPlay){
-            cout << i.first << " " << i.second << endl;
-        }
     }
-    cout << sum << endl;
+    cout << "Total matches : " << sum << endl;
     return 0;
 }
