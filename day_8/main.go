6542bcb4129359c7e979efc186da7510ed6be43c
niro58 <nichita.roilean@gmail.com>
2024-05-27 23:16:51 +0200

    lcm method poggers

--- day_8/main.go
+++ day_8/main.go
@@ -68,10 +68,36 @@ func calculateFirstPart(input string) int {
 	fmt.Println("Found the cuck in", index,"steps")
 	return 0
 }
+func GCD(a, b int) int {
+	for b != 0 {
+			t := b
+			b = a % b
+			a = t
+	}
+	return a
+}
+
+func LCM(a, b int, integers ...int) int {
+	result := a * b / GCD(a, b)
+
+	for i := 0; i < len(integers); i++ {
+			result = LCM(result, integers[i])
+	}
+
+	return result
+}
+func allElementsNotEqualToZero(arr []int) bool {
+    for _, value := range arr {
+		if value == 0 {
+			return false
+		}
+    }
+    return true
+}
 func calculateSecondPart(input string) int {
 	lines := strings.Split(input, "\r\n")
 
-	directions := make(map[string]Direction, len(lines) - 2)
+	rawDirections := make(map[string]Direction, len(lines) - 2)
 
 	commands := lines[0]
 
@@ -81,49 +107,49 @@ func calculateSecondPart(input string) int {
 		key := commaDelimiter[0][0:3]
 		leftValue := commaDelimiter[0][len(commaDelimiter[0]) - 3:len(commaDelimiter[0])]
 		rightValue := commaDelimiter[1][0:3]
-		writeDirection(key+ " = ( " + leftValue+" "+rightValue+ " )")
-		directions[key] = Direction{
+
+		rawDirections[key] = Direction{
 			leftValue,
 			rightValue,
 		}
 	}
-	var currentDirections []string
-	for key := range directions {
-		fmt.Println(key)
+
+	var selectedDirections []string
+
+	for key := range rawDirections {
 		if strings.HasSuffix(key, "A"){
-			currentDirections = append(currentDirections, key)
+			selectedDirections = append(selectedDirections, key)
 		}
 	}
-	fmt.Println("Commands", commands, len(commands))
+	selectedDirectionsSteps := make([]int,len(selectedDirections))
 
-	steps := 0
-
-	for {
-		for _,cmd := range commands{
-			isValid := true
-			for i, pos := range currentDirections {
-				dir,ok := directions[pos]
-				if (!ok){
-					fmt.Println(pos,ok)
+	step := 0
+	for !allElementsNotEqualToZero(selectedDirectionsSteps){
+		fmt.Println("----")
+		fmt.Println(selectedDirections)
+		fmt.Println(selectedDirectionsSteps)
+		for _, cmd := range commands{
+			for index, dir := range selectedDirections{
+				if strings.HasSuffix(dir, "Z"){
+					continue
 				}
+
+				nextDirection := rawDirections[dir]
+				var nextDestination string
 				if(cmd == 'L'){
-					currentDirections[i] = dir.left
+					nextDestination = nextDirection.left
 				}else{
-					currentDirections[i] = dir.right
+					nextDestination = nextDirection.right
 				}
-				if !strings.HasSuffix(currentDirections[i], "Z"){
-					isValid = false
+				selectedDirections[index] = nextDestination
+				if strings.HasSuffix(nextDestination, "Z"){
+					selectedDirectionsSteps[index] = step + 1
 				}
 			}
-			steps += 1
-			if steps % 10000000 == 0{
-				fmt.Println(steps)
-			}
-			if isValid{
-				return steps
-			}
+			step += 1;
 		}
 	}
+	return LCM(selectedDirectionsSteps[0],selectedDirectionsSteps[1],selectedDirectionsSteps[2:]...)
 }
 
 func main() {
