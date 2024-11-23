8562038d79d67deca2f8ef2da4bd91a79550bcfa
niro58 <nichita.roilean@gmail.com>
2024-11-24 00:18:36 +0100

    saving day_12 for later, day_13 tg

--- day_12/main.go
+++ day_12/main.go
@@ -47,56 +47,166 @@ func matchingOccs(a []int,toMatch []int, maxToMatch int) int{
 	}
 	return len(toMatch)
 }
-func processMemo(hash string, i int, validOccs []int, res *int, totalIter *int) {
-	*totalIter += 1
-
-	var maxOcc int
-	for i := range validOccs{
-		maxOcc = max(maxOcc, validOccs[i])
-	}
-
-	occ := calculateOccurences(hash)
-	position := matchingOccs(occ, validOccs, maxOcc)
-	if position == -1 {
-		return
-	}else if  !canFinish(validOccs,position, len(hash) - i){
-		fmt.Println("Hash",hash,"Position",position,"Valid occs",validOccs)
-		return
-	}
-	if position == len(validOccs) {
-		*res += 1
-		return
-	}
+type Hash struct{
+	Hash string
+	Index int
+}
+func getResult(hash string, validOccs []int ) string {
+	var q []Hash
+	q = append(q, Hash{
+		Hash: hash,
+		Index:0,
+	})
 	
-	if i == len(hash) {
-		return
+	var res string
+	for res == ""{
+		last := len(q) - 1
+		h := q[last]
+		q = q[:last]
+
+		var maxOcc int
+		for i := range validOccs{
+			maxOcc = max(maxOcc, validOccs[i])
+		}
+	
+		occ := calculateOccurences(h.Hash)
+		position := matchingOccs(occ, validOccs, maxOcc)
+		if position == -1 || !canFinish(validOccs,position, len(hash) - h.Index) {
+			continue
+		}
+
+		if position == len(validOccs) {
+			res = h.Hash
+			continue
+		}
+		
+		if h.Index == len(h.Hash) {
+			continue
+		}
+
+		q = append(q, Hash {
+			Hash:h.Hash,
+			Index: h.Index + 1,
+		})
+	
+		if hash[h.Index] != '?'{
+			continue
+		}
+	
+		hArr := []rune(h.Hash)
+		hArr[h.Index] = '#'
+		
+		q = append(q, Hash{
+			Hash: string(hArr),
+			Index: h.Index + 1,
+		})
 	}
-
-	processMemo(hash, i + 1, validOccs, res, totalIter)
-
-	if hash[i] != '?'{
-		return
+	return res
+}
+func isLocked(i int, lockedPositions []int) bool{
+	for j := range lockedPositions{
+		if lockedPositions[j] == i{
+			return true
+		}
 	}
+	return false
+}
+type OccMovement struct{
+	Left int
+	Right int
+	Distance int
+	Locked []int
 
-	hArr := []rune(hash)
-	hArr[i] = '#'
-
-	processMemo(string(hArr), i + 1, validOccs, res,totalIter)
+	LeftOffset int
+	RightOffset int
 }
 
-func processRow(hash string, validOccs []int) int{
+func CreateMovements(result string, hash string) []OccMovement{
+	var i int
+	var movements []OccMovement 
+
+	for i < len(result){
+		if(result[i] != '#'){
+			i += 1
+			continue
+		}
+		var movement OccMovement
+		
+		//Left
+		movement.Left = i
+
+		//Right and locked positions
+		movement.Right = i
+		for movement.Right < len(result) && result[movement.Right] == '#'{
+			if hash[movement.Right] == '#'{
+				movement.Locked = append(movement.Locked, movement.Right)
+			}
+			movement.Right += 1
+		}
+		movement.Right -= 1
+
+		//Distance
+		movement.Distance = movement.Right - movement.Left + 1
+		
+		
+		j := movement.Left - 1
+		for j >= 0{
+			if result[j] != '?' || (j - 1 >= 0 && result[j - 1] == '#') {
+				break;
+			}
+			movement.LeftOffset += 1
+			j -= 1 
+		}
+		j = movement.Right + 1
+		for j < len(result){
+			if result[j] != '?' || (j + 1 < len(result) && result[j + 1] == '#') {
+				break;
+			}
+			movement.RightOffset += 1
+			j += 1
+		}
+
+		movements = append(movements, movement)
+		i = movement.Right + 1
+	}
+	return movements
+}
+func (m OccMovement) Debug(){
+	fmt.Println("--------------")
+	fmt.Println("Left",m.Left)
+	fmt.Println("Right",m.Right)
+	fmt.Println("Distance",m.Distance)
+	fmt.Println("Locked",m.Locked)
+	fmt.Println("LeftOffset",m.LeftOffset)
+	fmt.Println("RightOffset",m.RightOffset)
+}
+func getTotalSolutions(movements []OccMovement, result int) int{
+	if len(movements) == 0 {
+		return result
+	}
+
+	movement := movements[0]
+	var currRes int
 	
-	var res int
-	//iter for testing
-	var iter int
+	return getTotalSolutions(movements, result * currRes)
+}
+func processRow(hash string, validOccs []int) int{
+	res := getResult(hash, validOccs)
+	occMovements := CreateMovements(res, hash)
+	fmt.Println("First result", res)
+	for _,j :=range occMovements{
+		j.Debug()
+	}
+	var distances []int
 
-	processMemo(hash, 0, validOccs, &res, &iter)
-
-	fmt.Println("!!!!!!!!")
-	fmt.Println(hash)
-	fmt.Println(res,iter)	
-	fmt.Println("---------")
-	return res
+	
+	var totalSolutions int = 1
+	//Solution
+	for _, move := range distances{
+		totalSolutions *= move + 1
+	}
+	fmt.Println(hash, totalSolutions)
+	return totalSolutions
 }
 func firstPart(input string) int{
 	rows := strings.Split(input, "\r\n")
@@ -123,7 +233,7 @@ func firstPart(input string) int{
 				fHashes += hash + "?"
 			}
 		}
-		resInt := processRow(fHashes, fOccs)
+		resInt := processRow(hash, occs)
 
 		total += resInt
 	}
