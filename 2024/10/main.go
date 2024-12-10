0e918c4beb2db44fe48dfb0ec36a231a42f7a2ca
niro58 <nichita.roilean@gmail.com>
2024-12-10 19:51:24 +0100

    2024-10 way too easy for day 10

---
+++ 2024/10/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+func move(start int, x int, y int, cols int, rows int) int {
+	newX := start % cols + x
+	newY := start / cols + y
+
+	if newX >= cols  || newX < 0|| newY < 0 || newY >= rows {
+		return -1
+	}
+	res := newX + newY * cols
+
+	return res
+}
+func includes(a []int, b int)bool{
+	for i := range a {
+		if a[i] == b{
+			return true
+		}
+	}
+	return false
+}
+func findSolutions(grid[]int, index,height,cols,rows int, results *int){
+	if height == 9 {
+		*results += 1
+		return
+	}
+	moves := []int{
+		move(index,1,0,cols,rows),
+		move(index,-1,0,cols,rows),
+		move(index,0,1,cols,rows),
+		move(index,0,-1,cols,rows),
+	}
+	for i := range moves{
+		if moves[i] == -1 {
+			continue
+		}
+		if grid[moves[i]] == height + 1{
+			findSolutions(grid, moves[i], height+1,cols,rows,results)
+		}
+	}
+}
+func firstPart(grid []int, cols,rows int) int {
+	var res int
+	for i := range grid{
+		if grid[i] == 0 {
+			var sols int
+
+			findSolutions(grid,i,0,cols,rows,&sols)
+
+			res += sols
+		}
+	}
+	return res
+}
+func main() {
+	start := time.Now()
+	input, _ := os.ReadFile(".\\input\\01.txt")
+	inputFlat := strings.Replace(string(input), "\r\n", "", -1)
+
+	var inputInt []int
+	for i := range inputFlat{
+		if inputFlat[i] == '.'{
+			inputInt = append(inputInt, 99)
+			continue
+		}
+		n, _ := strconv.Atoi(string(inputFlat[i]))
+		inputInt = append(inputInt, n)
+	}
+	rows := len(strings.Split(string(input), "\r\n"))
+	cols := len(strings.Split(string(input), "\r\n")[0])
+	res := firstPart(inputInt, cols,rows)
+	fmt.Println("Result" , res)
+
+	fmt.Println("Execution time: ", time.Since(start))
+}
