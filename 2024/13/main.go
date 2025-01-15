a291860849f1ff6f58d6e17e98335141d7f698f7
niro58 <nichita.roilean@gmail.com>
2025-01-15 15:06:39 +0100

    2024-13 all me :)

--- 2024/13/main.go
+++ 2024/13/main.go
@@ -6,36 +6,25 @@ import (
 	"strconv"
 	"strings"
 	"time"
-) 
-type Vector2 struct{
+)
+
+type Vector2 struct {
 	X int
 	Y int
 }
-func divmod(numerator, denominator int64) (quotient, remainder int64) {
-    quotient = numerator / denominator
-    remainder = numerator % denominator
-    return
-}
-func minTokens2(aButton, bButton, target Vector2)  int{
-	var curr Vector2
-	var tokens int
-	for curr.X < target.X && curr.Y < target.Y{
-		xDiv, xRem := divmod(int64(target.X - curr.X), int64(bButton.X))
-		yDiv, yRem := divmod(int64(target.Y - curr.Y), int64(bButton.Y))
-		
-		if xDiv == yDiv && xDiv != 0 && yDiv != 0 && xRem == 0 && yRem == 0{
-			return tokens + int(xDiv)
-		}
 
-		curr = Vector2{
-			curr.X + aButton.X,
-			curr.Y + aButton.Y,
+func minTokens(aButton, bButton, target Vector2) int {
+	leftSide := target.X*bButton.Y - target.Y*bButton.X
+	rightSide := aButton.X*bButton.Y - aButton.Y*bButton.X
+	fmt.Printf("(%d-(%d*x))/%d = (%d-(%d*x))/%d\r\n", target.X, aButton.X, bButton.X, target.Y, aButton.Y, bButton.Y)
+	if leftSide%rightSide == 0 {
+		res := leftSide / rightSide
+		target.X -= aButton.X * res
+		if  target.X  % bButton.X != 0{
+			return 0
 		}
-		tokens += 3
-
-	}
-	if curr == target{
-		return tokens
+		rem := target.X / bButton.X
+		return res*3 + rem
 	}
 	return 0
 }
@@ -46,32 +35,36 @@ func main() {
 	rows := strings.Split(string(input), "\r\n")
 	var total int
 	i := 0
-	for (i) * 4 < len(rows){
-		aButtonX,_ := strconv.Atoi(strings.Split(strings.Split(rows[i*4], "X")[1], ",")[0])
-		aButtonY,_ := strconv.Atoi(strings.Split(rows[i*4], "Y")[1])
+	for (i)*4 < len(rows) {
+		aButtonX, _ := strconv.Atoi(strings.Split(strings.Split(rows[i*4], "X")[1], ",")[0])
+		aButtonY, _ := strconv.Atoi(strings.Split(rows[i*4], "Y")[1])
 		aButton := Vector2{
 			X: aButtonX,
 			Y: aButtonY,
 		}
 
-		bButtonX,_ := strconv.Atoi(strings.Split(strings.Split(rows[i*4+1], "X")[1], ",")[0])
-		bButtonY,_ := strconv.Atoi(strings.Split(rows[i*4+1], "Y")[1])
+		bButtonX, _ := strconv.Atoi(strings.Split(strings.Split(rows[i*4+1], "X")[1], ",")[0])
+		bButtonY, _ := strconv.Atoi(strings.Split(rows[i*4+1], "Y")[1])
 		bButton := Vector2{
 			X: bButtonX,
 			Y: bButtonY,
 		}
 
-		tButtonX,_ := strconv.Atoi(strings.Split(strings.Split(rows[i*4+2], "X=")[1], ",")[0])
-		tButtonY,_ := strconv.Atoi(strings.Split(rows[i*4+2], "Y=")[1])
+		tButtonX, _ := strconv.Atoi(strings.Split(strings.Split(rows[i*4+2], "X=")[1], ",")[0])
+		tButtonY, _ := strconv.Atoi(strings.Split(rows[i*4+2], "Y=")[1])
 		target := Vector2{
-			X: tButtonX  ,
-			Y: tButtonY  ,
+			X: tButtonX + 10000000000000,
+			Y: tButtonY + 10000000000000,
 		}
-		res := minTokens(aButton,bButton, target)
+
+		res := minTokens(aButton, bButton, target)
+
 		total += res
-		fmt.Println("Res",res)
+		if res != 0 {
+			fmt.Println(tButtonX, res)
+		}
 		i += 1
 	}
-	fmt.Println("Total",total)
+	fmt.Println("Total", total)
 	fmt.Println("Execution time: ", time.Since(start))
 }
