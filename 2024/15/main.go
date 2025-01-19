e5fd5ccfa3a9ac93983b6263491a726bd066a44f
niro58 <nichita.roilean@gmail.com>
2025-01-20 00:30:00 +0400

    2024-15 cleanup

--- 2024/15/main.go
+++ 2024/15/main.go
@@ -1,5 +1,8 @@
 // 1514522 too low
 // 1521453 sheesh
+// 2.7067ms before cleanup
+// 1.6ms before cleanup
+
 package main
 
 import (
@@ -15,11 +18,25 @@ var robot Robot
 var WALL_RUNE = '#'
 var OBSTACLE_RUNE = 'O'
 var PLAYER_RUNE = '@'
-var RUNE_TO_GRID = map[rune]string{
-	WALL_RUNE:     "##",
-	OBSTACLE_RUNE: "[]",
-	PLAYER_RUNE:   "..",
-	'.':           "..",
+var EMPTY_RUNE = '.'
+var OBSTACLE_LEFT = '['
+var OBSTACLE_RIGHT = ']'
+
+var MOVE_TO_VECTOR = map[rune]Vector2{
+	'>': {
+		X: 1,
+		Y: 0,
+	},
+	'^': {
+		X: 0,
+		Y: -1,
+	}, 'v': {
+		X: 0,
+		Y: 1,
+	}, '<': {
+		X: -1,
+		Y: 0,
+	},
 }
 
 type Vector2 struct {
@@ -28,13 +45,6 @@ type Vector2 struct {
 }
 
 func (v Vector2) AddBasic(v2 Vector2) Vector2 {
-	res := Vector2{
-		X: v.X + v2.X,
-		Y: v.Y + v2.Y,
-	}
-	if res.X < 0 || res.Y < 0 {
-		fmt.Println("yo")
-	}
 	return Vector2{
 		X: v.X + v2.X,
 		Y: v.Y + v2.Y,
@@ -46,9 +56,9 @@ type Robot struct {
 }
 
 func IsObstacle(c rune) bool {
-	return c == rune(RUNE_TO_GRID[OBSTACLE_RUNE][0]) || c == rune(RUNE_TO_GRID[OBSTACLE_RUNE][1])
+	return c == OBSTACLE_LEFT || c == OBSTACLE_RIGHT
 }
-func Includes(arr []Vector2, el Vector2) bool {
+func (el Vector2) IncludedIn(arr []Vector2) bool {
 	for _, v := range arr {
 		if v == el {
 			return true
@@ -57,12 +67,12 @@ func Includes(arr []Vector2, el Vector2) bool {
 	return false
 }
 func (r *Robot) MoveY(v Vector2) {
-	oppositeSideMap := map[rune]Vector2{
-		'[': {
+	obstacleToOppositeVector := map[rune]Vector2{
+		OBSTACLE_LEFT: {
 			X: 1,
 			Y: 0,
 		},
-		']': {
+		OBSTACLE_RIGHT: {
 			X: -1,
 			Y: 0,
 		},
@@ -72,91 +82,104 @@ func (r *Robot) MoveY(v Vector2) {
 	var walked []Vector2
 
 	open = append(open, r.Position.AddBasic(v))
-	open = append(open, open[0].AddBasic(oppositeSideMap[grid[open[0].Y][open[0].X]]))
-	//rethink
+	open = append(open, open[0].AddBasic(obstacleToOppositeVector[grid[open[0].Y][open[0].X]]))
+
 	for len(open) > 0 {
 		curr := open[0]
 		open = open[1:]
-		if !Includes(walked, curr) {
+		if !curr.IncludedIn(walked) {
 			walked = append(walked, curr)
 		}
+
 		nextPos := curr.AddBasic(v)
+
 		c := grid[nextPos.Y][nextPos.X]
-		if c != '[' && c != ']' {
+		if !IsObstacle(c) {
 			continue
 		}
-		open = append(open, nextPos, nextPos.AddBasic(oppositeSideMap[c]))
-	}
-	canMove := true
-	for i := len(walked) - 1; i >= 0; i-- {
-		if grid[walked[i].Y+v.Y][walked[i].X] == WALL_RUNE {
-			canMove = false
-		}
-	}
-	if canMove {
-		for i := len(walked) - 1; i >= 0; i-- {
-			c := grid[walked[i].Y][walked[i].X]
 
-			grid[walked[i].Y][walked[i].X] = '.'
-			grid[walked[i].Y+v.Y][walked[i].X] = c
-		}
-		r.Position = r.Position.AddBasic(v)
+		open = append(open, nextPos, nextPos.AddBasic(obstacleToOppositeVector[c]))
 	}
+
+	for _, pos := range walked {
+		if grid[pos.Y+v.Y][pos.X] == WALL_RUNE {
+			return
+		}
+	}
+
+	for i := len(walked) - 1; i >= 0; i-- {
+		cell := walked[i]
+		c := grid[cell.Y][cell.X]
+
+		grid[cell.Y][cell.X] = '.'
+		grid[cell.Y+v.Y][cell.X] = c
+	}
+	r.Position = r.Position.AddBasic(v)
 
 }
 func (r *Robot) MoveX(v Vector2) {
-	newPos := r.Position.AddBasic(v)
-	posAfter := newPos.AddBasic(v)
-	for IsObstacle(grid[posAfter.Y][posAfter.X]) {
-		posAfter = posAfter.AddBasic(v)
+	nextPos := r.Position.AddBasic(v)
+
+	posAfterNext := nextPos.AddBasic(v)
+	for IsObstacle(grid[posAfterNext.Y][posAfterNext.X]) {
+		posAfterNext = posAfterNext.AddBasic(v)
 	}
 
-	if grid[posAfter.Y][posAfter.X] == '.' {
-		grid[newPos.Y][newPos.X] = '.'
-
-		diff := newPos.X - posAfter.X
-		if diff < 0 {
-			diff *= -1
-		}
-		for i := 0; i < diff; i++ {
-			if i%2 == 0 {
-				if v.X == 1 {
-					grid[newPos.Y][posAfter.X+(i*v.X*-1)] = ']'
-				} else {
-					grid[newPos.Y][posAfter.X+(i*v.X*-1)] = '['
-				}
-			} else {
-				if v.X == -1 {
-					grid[newPos.Y][posAfter.X+(i*v.X*-1)] = ']'
-				} else {
-					grid[newPos.Y][posAfter.X+(i*v.X*-1)] = '['
-				}
-			}
-		}
-	} else {
+	if grid[posAfterNext.Y][posAfterNext.X] == WALL_RUNE {
 		return
 	}
 
-	r.Position = newPos
+	grid[nextPos.Y][nextPos.X] = '.'
+
+	distance := nextPos.X - posAfterNext.X
+	if distance < 0 {
+		distance *= -1
+	}
+
+	var c rune
+	if v.X < 0 {
+		c = OBSTACLE_LEFT
+	} else if v.X > 0 {
+		c = OBSTACLE_RIGHT
+	}
+	for i := 0; i < distance; i++ {
+		grid[nextPos.Y][posAfterNext.X+(i*v.X*-1)] = c
+
+		if c == OBSTACLE_LEFT {
+			c = OBSTACLE_RIGHT
+		} else {
+			c = OBSTACLE_LEFT
+		}
+	}
+
+	r.Position = nextPos
 }
 func (r *Robot) Move(v Vector2) {
-	newPos := r.Position.AddBasic(v)
-	curr := grid[newPos.Y][newPos.X]
-	if curr == WALL_RUNE || newPos.X < 0 {
+	nextPos := r.Position.AddBasic(v)
+	curr := grid[nextPos.Y][nextPos.X]
+	if curr == WALL_RUNE {
 		return
 	}
 	if !IsObstacle(curr) {
-		r.Position = newPos
+		r.Position = nextPos
 		return
 	}
 
 	if v.X != 0 {
 		r.MoveX(v)
-	} else {
+	}
+	if v.Y != 0 {
 		r.MoveY(v)
 	}
 }
 func GetGrid(input string) (grid [][]rune, robotPosition Vector2, lastRow int) {
+	RUNE_TO_GRID := map[rune]string{
+		WALL_RUNE:     string([]rune{WALL_RUNE, WALL_RUNE}),
+		OBSTACLE_RUNE: string([]rune{OBSTACLE_LEFT, OBSTACLE_RIGHT}),
+		PLAYER_RUNE:   string([]rune{EMPTY_RUNE, EMPTY_RUNE}),
+		EMPTY_RUNE:    string([]rune{EMPTY_RUNE, EMPTY_RUNE}),
+	}
+
 	for y, row := range strings.Split(input, "\r\n") {
 		grid = append(grid, []rune{})
 		if len(row) == 0 {
@@ -185,7 +208,7 @@ func GetMoves(input string, lastRow int) []rune {
 	}
 	return moves
 }
-func PreviewGrid(robot Robot) {
+func PreviewGrid() {
 	for y, row := range grid {
 		for x, cell := range row {
 			if robot.Position.Y == y && robot.Position.X == x {
@@ -200,33 +223,14 @@ func PreviewGrid(robot Robot) {
 func Calculate(input string) int {
 	g, robotPosition, lastRow := GetGrid(input)
 	moves := GetMoves(input, lastRow)
-
 	grid = g
 	robot = Robot{
 		Position: robotPosition,
 	}
 	for len(moves) > 0 {
-
 		move := moves[0]
 		moves = moves[1:]
-
-		var v Vector2
-		if move == '>' {
-			v.X = 1
-		} else if move == '^' {
-			v.Y = -1
-		} else if move == '<' {
-			v.X = -1
-		} else if move == 'v' {
-			v.Y = 1
-		} else {
-			panic(move)
-		}
-
-		// fmt.Println(string(move))
-		robot.Move(v)
-		// PreviewGrid(robot)
-		// fmt.Println()
+		robot.Move(MOVE_TO_VECTOR[move])
 	}
 
 	var res int
@@ -241,7 +245,7 @@ func Calculate(input string) int {
 }
 func main() {
 	start := time.Now()
-	input, err := os.ReadFile("./input/02.txt")
+	input, err := os.ReadFile("./input/01.txt")
 	if err != nil {
 		panic(err)
 	}
